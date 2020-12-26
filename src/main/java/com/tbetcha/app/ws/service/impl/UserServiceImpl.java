a87762e4d73705c4a50a853f115e77d8120606c1
TMB <Troy.Boettger@gmail.com>
2020-12-26 15:37:36 -0500

    add utils file and implement random ids

--- src/main/java/com/tbetcha/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/tbetcha/app/ws/service/impl/UserServiceImpl.java
@@ -3,10 +3,11 @@ package com.tbetcha.app.ws.service.impl;
 import com.tbetcha.app.ws.UserRepository;
 import com.tbetcha.app.ws.io.entity.UserEntity;
 import com.tbetcha.app.ws.service.UserService;
+import com.tbetcha.app.ws.shared.Utils;
+import com.tbetcha.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.tbetcha.app.ws.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,14 +15,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserID(30);
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
 
         UserEntity storedUserDetails =  userRepository.save(userEntity);
 
