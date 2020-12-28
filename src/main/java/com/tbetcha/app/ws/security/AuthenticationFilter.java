3ff8b1fb7ec5f888cd6e2fa82cb1c5f2cfa22ee8
TMB <Troy.Boettger@gmail.com>
2020-12-27 23:16:31 -0500

    add more for attempted authentication

--- src/main/java/com/tbetcha/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/tbetcha/app/ws/security/AuthenticationFilter.java
@@ -46,7 +46,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
                                             Authentication auth  ) throws IOException, ServletException {
         String userName = ((User) auth.getPrincipal()).getUsername();
-        //String tokenSecret = new SecurityConstants.getToeknSecret();
+        //String tokenSecret = new SecurityConstants.getTokenSecret();
 
         String token =
                 Jwts.builder().setSubject(userName).setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME)).signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET).compact();
