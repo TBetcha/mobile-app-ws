eba10658c7542030221769050150e5144c27ebbd
TMB <Troy.Boettger@gmail.com>
2020-12-31 05:50:49 -0500

    remove hard coded token secret and add method to retrieve

--- src/main/java/com/tbetcha/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/tbetcha/app/ws/security/AuthenticationFilter.java
@@ -52,7 +52,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         //String tokenSecret = new SecurityConstants.getTokenSecret();
 
         String token =
-                Jwts.builder().setSubject(userName).setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME)).signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET).compact();
+                Jwts.builder().setSubject(userName).setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME)).signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret()).compact();
 
         UserService userService = (UserService) SpringApplicationContext.getBean("userServiceImpl");
        UserDto userDto = userService.getUser(userName);
